bdc4ed4dcdae6ef9708349d429a497eb615f7269
analogsoul13 <analogsoul13@gmail.com>
2024-10-27 23:39:23 +0530

    Data Display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function Header() {
+
+    const {books} = useSelector((state)=> state.books)
     return (
         <>
             {/* Navbar */}
             <div className="navbar p-2">
                 <div className="flex-1">
                     <Link to={'/'}><button className="btn btn-ghost text-gray-900 text-2xl">Redux CRUD Library</button></Link>
+                </div>
+                <div className="p-2">
+                    Books
+                    <div className="badge ml-2 badge-neutral">{books.length}+</div>
                 </div>
                 <div className='flex-none sm:hidden'>
                     <label className="btn btn-ghost bg-white swap swap-rotate">
